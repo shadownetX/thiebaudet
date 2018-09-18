a198620340df7863654efc99eec9033e776428e6
vonP <thomas@thiebaudet.fr>
2018-09-18 22:44:20 +0200

    feat(entities) create entities for begining

---
+++ src/Entity/Article.php
@@ -0,0 +1,261 @@
+<?php
+
+/**
+ * This file is part of the Thiebaudet package.
+ *
+ * (c) thiebaudet.fr <thomas@thiebaudet.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Article du blog.
+ *
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * Titre de l'article.
+     *
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, unique=true)
+     *
+     * @Assert\NotBlank()
+     */
+    private $title;
+
+    /**
+     * Sous-titre de l'article.
+     *
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, unique=true)
+     *
+     * @Assert\NotBlank()
+     */
+    private $subtitle;
+
+    /**
+     * Slug de l'article.
+     *
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, unique=true)
+     *
+     * @Assert\NotBlank()
+     */
+    private $slug;
+
+    /**
+     * Corps de l'article.
+     *
+     * @var string
+     *
+     * @ORM\Column(type="text")
+     *
+     * @Assert\NotBlank()
+     */
+    private $body;
+
+    /**
+     * L'article est-il en ligne ?
+     *
+     * @var boolean
+     *
+     * @ORM\Column(type="boolean")
+     */
+    private $online = false;
+
+    /**
+     * Date de création de l'article.
+     *
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime")
+     *
+     * @Assert\DateTime()
+     */
+    private $createdAt;
+
+    /**
+     * Date de modification de l'article.
+     *
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime")
+     *
+     * @Assert\DateTime()
+     */
+    private $updatedAt;
+
+    public function updatedTimestamps()
+    {
+        $this->setUpdatedAt(new \DateTime('now'));
+
+        if (null == $this->getCreatedAt()) {
+            $this->setCreatedAt(new \DateTime('now'));
+        }
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     *
+     * @return Article
+     */
+    public function setTitle(string $title): Article
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSubtitle(): string
+    {
+        return $this->subtitle;
+    }
+
+    /**
+     * @param string $subtitle
+     *
+     * @return Article
+     */
+    public function setSubtitle(string $subtitle): Article
+    {
+        $this->subtitle = $subtitle;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param string $slug
+     *
+     * @return Article
+     */
+    public function setSlug(string $slug): Article
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody(): string
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param string $body
+     *
+     * @return Article
+     */
+    public function setBody(string $body): Article
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isOnline(): bool
+    {
+        return $this->online;
+    }
+
+    /**
+     * @param bool $online
+     *
+     * @return Article
+     */
+    public function setOnline(bool $online): Article
+    {
+        $this->online = $online;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     *
+     * @return Article
+     */
+    public function setCreatedAt(\DateTime $createdAt): Article
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTime $updatedAt
+     *
+     * @return Article
+     */
+    public function setUpdatedAt(\DateTime $updatedAt): Article
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
